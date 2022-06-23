2729637422ee4725505e224a8e540e7c9fd46dd9
Angel Nápoles Nápoles <69412170+angelqva@users.noreply.github.com>
2022-06-23 00:47:01 -0400

    register models
    register models in admin dashboard

--- estudiantes/admin.py
+++ estudiantes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from estudiantes.models import Estudiante
 # Register your models here.
+admin.site.register(Estudiante)
--- postgrados/admin.py
+++ postgrados/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from postgrados.models import Nacional, Internacional
 # Register your models here.
+admin.site.register(Nacional)
+admin.site.register(Internacional)
